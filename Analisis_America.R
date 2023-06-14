8480fbd4058303675ef935514849ee4d5b26dbfa
Alberto Tavera <atavera.galicia@gmail.com>
2023-06-13 18:49:21 -0600

    Se agregaron cambios a las graficas

--- Analisis_America.R
+++ Analisis_America.R
@@ -1,9 +1,10 @@
 #El presente proyecto tiene como objetivo explorar los
 #Resultados del Club América del año 2013 al 2023
+#install.packages("wesanderson")
 
 library(data.table)
 library (tidyverse)
-library(ggplot2)
+library(wesanderson)
 
 setwd("~/GitHub/CAFC")
 
@@ -84,20 +85,50 @@ america <- america %>%
   group_by(Season) %>% 
   mutate(PPganados = JuGan / JueJu)
 
+
+#Creando funcion para reemplazar un valor en un a columna. 
+
+reemplazar_valor <- function(data, variable, valor_original, valor_reemplazo) {
+  data[[variable]] <- ifelse(data[[variable]] == valor_original, 
+                             valor_reemplazo, data[[variable]])
+  return(data)
+}
+
+#A continuacion se reemplaza el nombre de los equipos por uno mas corto.
+
+america <- reemplazar_valor(america, "Rival", "Atl. San Luis", "San Luis")
+america <- reemplazar_valor(america, "Rival", "Club Leon", "Leon")
+america <- reemplazar_valor(america, "Rival", "Club Tijuana", "Tijuana")
+america <- reemplazar_valor(america, "Rival", "Dorados de Sinaloa", "Dorados")
+america <- reemplazar_valor(america, "Rival", "Guadalajara Chivas", "Chivas")
+america <- reemplazar_valor(america, "Rival", "Lobos BUAP", "BUAP")
+america <- reemplazar_valor(america, "Rival", "Mazatlan FC", "Mazatlan")
+america <- reemplazar_valor(america, "Rival", "Santos Laguna", "Santos")
+america <- reemplazar_valor(america, "Rival", "U.A.N.L.- Tigres", "Tigres")
+america <- reemplazar_valor(america, "Rival", "U.N.A.M.- Pumas", "Pumas")
+america <- reemplazar_valor(america, "Rival", "Leones Negros", "UDG")
+
+
+
 #------------------------------------------------------------------------------#
 #############--En Esta sección se encuentran los graficos--##################### 
 
 #Las siguientes lineas crean un grafico de la proporcion de partidos ganados 
 #en cada año futbolistico.
 Plot_PPganados <- ggplot(america, aes(x=Season, y=PPganados, group=1))+
+  geom_line(color = wes_palette(n=1, name = "Moonrise1"))+
+  geom_point(color = wes_palette(n=1, name = "Moonrise1"))+
   ggtitle("Proporción de Partidos Ganados por Año Futbolístico")+
-  theme(plot.title=element_text(size=20, face='bold', color='Black',hjust=0.5),
-        axis.title.x=element_text(size=15, face='bold', color='Black'),
-        axis.title.y=element_text(size=15, face='bold', color='Black'))+
   xlab('Año Futbolístico') +
   ylab('Proporción Partidos Ganados')+
-  geom_line()+
-  geom_point()
+  theme(panel.background = element_rect(fill = "#000000"),
+        plot.background = element_rect(fill = "#000000"),
+        panel.grid = element_line(color="#272829"),
+        plot.title=element_text(hjust=0.5, color="#f0f8ff", family='mono', size=20),
+        axis.title.x=element_text(color="#f0f8ff", family='mono', size=15),
+        axis.title.y=element_text(color="#f0f8ff", family='mono', size=15),
+        axis.text = element_text(color="#f0f8ff", family='mono', size=10))
+ 
 
 Plot_PPganados
 ggsave(filename = "Plot_PPganados.pdf",
@@ -107,16 +138,21 @@ ggsave(filename = "Plot_PPganados.pdf",
 #Las siguientes lineas crean un grafico de los partidos ganados 
 #en cada año futbolistico.
 Plot_JuGan <- ggplot(america, aes(x=Season, y=JuGan, group=1))+
+  geom_line(color = wes_palette(n=1, name = "Moonrise1"))+
+  geom_point(color = wes_palette(n=1, name = "Moonrise1"))+
   ggtitle("Partidos Ganados por Año Futbolístico")+
-  theme(plot.title=element_text(size=20, face='bold', color='Black',hjust=0.5),
-        axis.title.x=element_text(size=15, face='bold', color='Black'),
-        axis.title.y=element_text(size=15, face='bold', color='Black'))+
   xlab('Año Futbolístico') +
   ylab('Partidos Ganados')+
-  geom_line()+
-  geom_point()
-
+  theme(panel.background = element_rect(fill = "#000000"),
+        plot.background = element_rect(fill = "#000000"),
+        panel.grid = element_line(color="#272829"),
+        plot.title=element_text(hjust=0.5, color="#f0f8ff", family='mono', size=20),
+        axis.title.x=element_text(color="#f0f8ff", family='mono', size=15),
+        axis.title.y=element_text(color="#f0f8ff", family='mono', size=15),
+        axis.text = element_text(color="#f0f8ff", family='mono', size=10))
+  
 Plot_JuGan
+
 ggsave(filename = "Plot_JuGan.pdf",
       width =40 , height = 20, units = "cm", dpi = 600)
 
@@ -127,13 +163,28 @@ ggsave(filename = "Plot_JuGan.pdf",
 america <- america[order(america$Rival),]
 
 # Graficar utilizando ggplot2
-Plot_Equipos <- ggplot(america, aes(x = Rival,  group=1)) +
-  geom_point(aes(y = JuGanRival, color = "Ganados")) +
-  geom_point(aes(y = JuPerRival, color = "Perdidos")) +
-  geom_point(aes(y = JuEmpRival, color = "Empatados")) +
+
+Plot_Equipos <- ggplot(america, aes(x = Rival, group = 1)) +
+  geom_point(aes(x = Rival, y = JuGanRival, color = "Ganados")) +
+  geom_point(aes(x = Rival, y = JuPerRival, color = "Perdidos")) +
+  geom_point(aes(x = Rival, y = JuEmpRival, color = "Empatados")) +
   geom_line(aes(y = JuGanRival, color = "Ganados")) +
   geom_line(aes(y = JuPerRival, color = "Perdidos")) +
   geom_line(aes(y = JuEmpRival, color = "Empatados")) +
-  labs(x = "Rivales", y = "Resultados", color = "Resultado") 
+  labs(x = "Rivales", y = "Resultados", color = " ", 
+       title='Resultados Club América Últimos 10 Años') +
+  scale_color_manual(values = wes_palette(n = 3, name = "Darjeeling1"))+
+  theme(panel.background = element_rect(fill = "#000000"),
+        plot.background = element_rect(fill = "#000000"),
+        panel.grid = element_line(color="#272829"),
+        axis.title.x = element_text(color="#f0f8ff", family='mono', size=15),
+        axis.title.y = element_text(color="#f0f8ff", family='mono', size=15),
+        plot.title=element_text(hjust=0.5, color="#f0f8ff", family='mono', size=20),
+        axis.text = element_text(color="#f0f8ff", family='mono', size=10),
+        legend.text = element_text(color="#f0f8ff", family='mono', size = 11),
+        legend.background = element_rect (fill= "#000000"))
+
 Plot_Equipos
 
+ggsave(filename = "Plot_Equipos.pdf",
+       width =40 , height = 20, units = "cm", dpi = 600)
